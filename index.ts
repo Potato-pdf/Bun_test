414f43e777830ce55e97a74ce906201ccd3c630a
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2026-05-11 11:40:05 -0600

    fea: bun async test

--- index.ts
+++ index.ts
@@ -2,4 +2,13 @@ export function sum(a: number, b: number): number {
     return a + b
 }
 
+export async function myasyncfunction() {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(true)
+        }, 10000)
+    })
+    return promise
+}
+
 
