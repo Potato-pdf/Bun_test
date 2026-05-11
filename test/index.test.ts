e385d23d89d1cc33e0c1091180655cbe6d11d3d1
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2026-05-11 11:40:12 -0600

    feat: test async function

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,9 +1,16 @@
 import { describe, test, expect } from "bun:test";
-import {sum} from "../index"
+import { myasyncfunction, sum } from "../index"
 
-describe("index.ts", ()=> {
-    test("2 + 2 = 4", ()=> {
+describe("index.ts", () => {
+    test("2 + 2 = 4", () => {
         expect(sum(2, 2)).toBe(4)
+    })
+
+    test("async function", (done) => {
+        myasyncfunction().then((value) => {
+            expect(value).toBe(true)
+            done()
+        })
     })
 })
 
